50bd72d44edf8c52f95dd3852b70cfadbc29c6d8
Pham Tien Dat <tdat9663@gmail.com>
2024-09-29 18:14:32 +0700

    create a article

--- resolvers.ts
+++ resolvers.ts
@@ -1,14 +1,31 @@
- //resolvers: la ham xu ly tuong ung khi fe goi vao cac bien
+//resolvers: la ham xu ly tuong ung khi fe goi vao cac bien
 
 import Article from "./models/article.model"
 
- export const resolvers = {
+export const resolvers = {
   Query: {
-    getListArticle:async()=>{
-      const articles= await Article.find({
-        deleted:false
+    getListArticle: async () => {
+      const articles = await Article.find({
+        deleted: false
       })
       return articles
+    },
+    getArticle: async (_, args) => {
+      //args giong nhu req.body
+      const id = args.id
+      const article = await Article.findOne({
+        _id: id,
+        deleted: false
+      })
+      return article
+    }
+  },
+  Mutation:{
+    createArticle:async(_,args)=>{
+      const article=args.article
+      const newArticle= new Article(article);
+      await newArticle.save();
+      return newArticle
     }
   }
 }
--- typedefs.ts
+++ typedefs.ts
@@ -1,17 +1,34 @@
-import {  gql } from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
 
-  //typdefs : chi dinh cac ham ma fe muon lay
-  //type Query : dinh nghia ra cac truong fe duoc lay mac dinh phai co
-  //type _name : dinh nghia cac truong tra ve cho cac bien trong Query, tuong tu interface typeScript, cac bien fe co the lay
-export  const typeDefs = gql`
+//typdefs : chi dinh cac ham ma fe muon lay
+//type Query : dinh nghia ra cac truong fe duoc lay mac dinh phai co
+//type _name : dinh nghia cac truong tra ve cho cac bien trong Query, tuong tu interface typeScript, cac bien fe co the lay
+
+//type Mutation : them sua xoa
+export const typeDefs = gql`
     type Article{
         id:String,
         title:String,
         avatar:String,
         description:String
     }
-
     type Query {
-      getListArticle:[Article]
+      getListArticle:[Article],
+      getArticle (id:String):Article
+    }
+
+
+
+
+
+
+
+    input ArticleInput{
+        title:String,
+        avatar:String,
+        description:String
+    }
+    type Mutation{
+      createArticle(article:ArticleInput):Article
     }
   `;
