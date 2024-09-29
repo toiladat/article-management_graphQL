867c2faac9e3cdee6c0f923b135b9855fcbb226a
Pham Tien Dat <tdat9663@gmail.com>
2024-09-29 17:45:46 +0700

    get articleList

--- resolvers.ts
+++ resolvers.ts
@@ -1,9 +1,14 @@
  //resolvers: la ham xu ly tuong ung khi fe goi vao cac bien
 
+import Article from "./models/article.model"
+
  export const resolvers = {
   Query: {
-    hello: () => {
-      return ("hello word");
+    getListArticle:async()=>{
+      const articles= await Article.find({
+        deleted:false
+      })
+      return articles
     }
   }
 }
--- typedefs.ts
+++ typedefs.ts
@@ -1,9 +1,17 @@
 import {  gql } from 'apollo-server-express';
 
   //typdefs : chi dinh cac ham ma fe muon lay
-  //type Query : dinh nghia ra cac truong fe duoc lay
+  //type Query : dinh nghia ra cac truong fe duoc lay mac dinh phai co
+  //type _name : dinh nghia cac truong tra ve cho cac bien trong Query, tuong tu interface typeScript, cac bien fe co the lay
 export  const typeDefs = gql`
+    type Article{
+        id:String,
+        title:String,
+        avatar:String,
+        description:String
+    }
+
     type Query {
-      hello:String
+      getListArticle:[Article]
     }
   `;
