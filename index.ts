39f893346288a043858465eb05ef01646a9ecf5b
Pham Tien Dat <tdat9663@gmail.com>
2024-09-29 16:41:36 +0700

    Ket noi database

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
+import  dotenv  from 'dotenv';
 import express, {Express,Request,Response} from 'express'
-
+import {connectDatabase}  from './config/database'
+import Article from './models/article.model'
+dotenv.config();
 const app:Express=express();
 const port:number|string=3000;
+connectDatabase();
 
-app.get('/articles',(req:Request,res:Response)=>{
+app.get('/articles',async (req:Request,res:Response)=>{
+  const articles=await Article.find({
+    deleted:false
+  })
   res.json({
-    articles:[]
+    articles:articles
   })
 })
 
